ac6a0efcd03b533e4d98f11354b499f5f7cacccc
Endriyas <endriyas@uni.minerva.edu>
2024-06-06 22:54:19 +0300

    auth middleware

---
+++ middleware/authentication.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+const { UnauthenticatedError } = require("../errors");
+
+const auth = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("bearer")) {
+    throw new UnauthenticatedError("Invalid Authentication");
+  }
+  const token = authHeader.split("")[1];
+
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = { userId: payload.userId, name: payload.name };
+    next();
+  } catch (error) {
+    throw new UnauthenticatedError("Invalid Authentication");
+  }
+};
+
+module.exports = auth;
